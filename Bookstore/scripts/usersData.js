3aff7add7016c6d301d38353def8a9ce0b9d2343
Kiril_Velichkov <kvelichkow@gmail.com>
2016-09-20 20:54:07 +0300

    added TO DOs fixed type in scripts/usersData.js

--- Bookstore/scripts/usersData.js
+++ Bookstore/scripts/usersData.js
@@ -37,7 +37,7 @@ var register = function(newUser){
     var promise = new Promise(function(resolve, reject){
         var requestedUser = {
             username: newUser.username,
-            password: UTILS.ecryptToSha1(newUser.password)
+            password: UTILS.encryptToSha1(newUser.password)
         };
 
         var autorizationHeader = UTILS.encryptToBase64(CONSTANTS.AUTORIZATION_STRING);
