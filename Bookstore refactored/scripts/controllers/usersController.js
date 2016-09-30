7b37ef654933cbfb5a385e97f353a528a7b2b4f0
Georgi Velikov <gmvelikov@gmail.com>
2016-09-30 16:28:20 +0300

    symbols validation added

--- Bookstore refactored/scripts/controllers/usersController.js
+++ Bookstore refactored/scripts/controllers/usersController.js
@@ -63,12 +63,20 @@ class UsersController {
                         password: $('#tb-newPassword').val()
                     };
 
-                    if(newUser.username.length < 3 || newUser.username.length > 20){
+                    if(newUser.username.length < CONSTANTS.USERNAME_MIN_LENGTH || newUser.username.length > CONSTANTS.USERNAME_MAX_LENGTH){
                         toastr.error('Username must be between 3 and 20 symbols');
                         return;
                     }
-                    if(newUser.password.length < 3 || newUser.password.length > 20){
+                    if(newUser.password.length < CONSTANTS.PASSWORD_MIN_LENGTH || newUser.password.length > CONSTANTS.PASSWORD_MAX_LENGTH){
                         toastr.error('Password must be between 3 and 20 symbols');
+                        return;
+                    }
+                    if(/\W+/.test(newUser.username)){
+                        toastr.error('Username contains invalid symbols');
+                        return;
+                    }
+                    if(/\W+/.test(newUser.password)){
+                        toastr.error('Password contains invalid symbols');
                         return;
                     }
 
